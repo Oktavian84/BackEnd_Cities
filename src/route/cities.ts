877ca8dbc80a3c0674e5994320c468e39089aa50
Oktavian Paunku <oggy134@hotmail.com>
2025-02-05 18:14:36 +0100

    Created more validations rules

--- src/route/cities.ts
+++ src/route/cities.ts
@@ -9,31 +9,55 @@ router.get("/", index);
 router.get("/:cityId", show);
 
 router.post("/", [
+	body("img_url")
+	.isString().withMessage("has to be a string").bail()
+	.trim().isLength({ min: 3, max: 191 }).withMessage("has to be 3-191 chars long"),
+
     body("city_name")
-		.isString().withMessage("has to be a string").bail()
-		.trim().isLength({ min: 3, max: 191 }).withMessage("has to be 3-191 chars long")
+	.isString().withMessage("has to be a string").bail()
+	.trim().isLength({ min: 3, max: 191 }).withMessage("has to be 3-191 chars long"),
+
+	body("city_location")
+	.isString().withMessage("title has to be a string").bail()
+	.trim().isLength({ min: 3, max: 191 }).withMessage("title has to be 3-191 chars"),
+
+	body("city_population")
+	.optional()
+	.isString().withMessage("title has to be a string").bail()
+	.trim().isLength({ min: 3, max: 191 }).withMessage("title has to be 3-191 chars"),
+		
+	body("city_description")
+	.optional()
+	.isString().withMessage("title has to be a string").bail()
+	.trim().isLength({ min: 3, max: 191 }).withMessage("title has to be 3-191 chars")
+
 ], store);
 
 router.patch("/:cityId", [
+	body("img_url")
+	.optional()
+	.isString().withMessage("has to be a string").bail()
+	.trim().isLength({ min: 3, max: 191 }).withMessage("has to be 3-191 chars long"),
+
     body("city_name")
-		.optional()
-		.isString().withMessage("has to be a string").bail()
-		.trim().isLength({ min: 3, max: 191 }).withMessage("has to be 3-191 chars long"),
+	.optional()
+	.isString().withMessage("has to be a string").bail()
+	.trim().isLength({ min: 3, max: 191 }).withMessage("has to be 3-191 chars long"),
 
-		body("city_location")
-		.optional()
-		.isString().withMessage("title has to be a string").bail()
-		.trim().isLength({ min: 3, max: 191 }).withMessage("title has to be 3-191 chars"),
+	body("city_location")
+	.optional()
+	.isString().withMessage("title has to be a string").bail()
+	.trim().isLength({ min: 3, max: 191 }).withMessage("title has to be 3-191 chars"),
 
-		body("city_population")
-		.optional()
-		.isString().withMessage("title has to be a string").bail()
-		.trim().isLength({ min: 3, max: 191 }).withMessage("title has to be 3-191 chars"),
+	body("city_population")
+	.optional()
+	.isString().withMessage("title has to be a string").bail()
+	.trim().isLength({ min: 3, max: 191 }).withMessage("title has to be 3-191 chars"),
 		
-		body("city_description")
-		.optional()
-		.isString().withMessage("title has to be a string").bail()
-		.trim().isLength({ min: 3, max: 600 }).withMessage("title has to be 3-600 chars")
+	body("city_description")
+	.optional()
+	.isString().withMessage("title has to be a string").bail()
+	.trim().isLength({ min: 3, max: 191 }).withMessage("title has to be 3-191 chars")
 
 ], update);
 
--- src/types/citiestypes.ts
+++ src/types/citiestypes.ts
@@ -2,6 +2,8 @@ export type CreateCity = {
     img_url: string;
     city_name: string;
     city_location: string;
+    city_population?: string;
+    city_description?: string;
 }
 
 export type UpdateCityData = Partial<CreateCity>;
